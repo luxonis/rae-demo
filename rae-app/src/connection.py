a71c31c6c35e06ea61fd6b3381cc2a0d008c8ffe
Daniel Zvara <dan.zvara@gmail.com>
2024-05-07 12:07:35 +0200

    Fix connection buffer size

--- rae-app/src/connection.py
+++ rae-app/src/connection.py
@@ -15,20 +15,21 @@ class Connection:
         self.connection = webrtc_python.WebRTC(self.config)
 
     def accept(self):
-        self.connection.accept_connection(10)
-        webrtc_data_channel = self.connection.create_data_channel("track")
+        connection_handle = self.connection.accept_connection(10)
+        webrtc_data_channel = connection_handle.create_data_channel("track")
         self.channels.append(webrtc_data_channel)
 
     def broadcast(self, data: bytes):
+        chunk_size = 16 * 1024 - 8  # 16KB - 8 bytes for header
         content_length = len(data)
         chunk_idx = 0
-        n_chunks = (content_length // (2**14 - 2)) + 1
+        n_chunks = (content_length // chunk_size) + 1
 
         while chunk_idx < n_chunks:
             header = chunk_idx.to_bytes(4, "big") + n_chunks.to_bytes(4, "big")
 
-            chunk_start_idx = chunk_idx * 2**14
-            chunk_end_idx = min((chunk_idx + 1) * 2**14, content_length)
+            chunk_start_idx = chunk_idx * chunk_size
+            chunk_end_idx = min((chunk_idx + 1) * chunk_size, content_length)
 
             chunk = data[chunk_start_idx:chunk_end_idx]
             chunk = header + chunk
